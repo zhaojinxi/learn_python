1ebb7dd092ec8cd486f3c680b4fd6e6a0a968b72
zhao <super-string@outlook.com>
2018-05-02 00:22:42 +0800

    6

--- compete_tianchi/美年健康AI大赛/prepare_data.py
+++ compete_tianchi/美年健康AI大赛/prepare_data.py
@@ -27,10 +27,31 @@ import numpy
 # part1.to_pickle('data/part1p')
 # part2.to_pickle('data/part2p')
 
-part1=pandas.read_pickle('data/part1p')
-part2=pandas.read_pickle('data/part2p')
-table_id1=part1['table_id'].drop_duplicates().sort_values().as_matrix().tolist()
-table_id2=part2['table_id'].drop_duplicates().sort_values().as_matrix().tolist()
+# part1p=pandas.read_pickle('data/part1p')
+# part2p=pandas.read_pickle('data/part2p')
+# data=pandas.concat([part1p,part2p],ignore_index=True)
+# data=data.drop_duplicates()
+# data.to_pickle('data/data_drop')
+
+# data=pandas.read_pickle('data/data_drop')
+# gb=data.groupby(by=['vid','table_id'])
+# t=0
+# k=pandas.DataFrame(columns=['vid','table_id','field_result'])
+# for x in gb:
+#     if x[1].shape[0]>1:
+#         k=k.append(x[1])
+#     print(t)
+#     t=t+1
+# k.to_pickle('data/duplicate')
+# data=data.drop(duplicate.index)
+# data.to_pickle('data/unduplicate')
+
+unduplicate=pandas.read_pickle('data/unduplicate')
+duplicate=pandas.read_pickle('data/duplicate')
+
+duptableid=duplicate['table_id'].drop_duplicates().sort_values().as_matrix().tolist()
+for x in duptableid:
+    print(unduplicate[unduplicate['table_id']==x]['field_result'])
 all=numpy.zeros([part1['vid'].drop_duplicates().shape[0], len(table_id1)])
 new_part1=pandas.DataFrame(all, index=part1['vid'].drop_duplicates().sort_values().as_matrix(), columns=table_id1)
 gb=part1.groupby(by=['vid'])
