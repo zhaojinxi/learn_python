0df6aa704fe4878601f240223d68a3f2a6cfe9a9
zhaojinxi <super-string@outlook.com>
2018-07-17 18:41:14 +0800

    6

--- compete_tianchi/全球气象AI挑战赛/cnn_convgru/predict.py
+++ compete_tianchi/全球气象AI挑战赛/cnn_convgru/predict.py
@@ -2,10 +2,17 @@ import tensorflow
 import os
 import cv2
 import numpy
+import time
+import zipfile
+import skimage.io
 
 data_dir='/media/zhao/新加卷/SRAD2018/test/SRAD2018_Test_1'
 log_dir='log/'
 model_dir='model/'
+
+predict_dir=os.path.join(os.path.split(data_dir)[0],'predict')
+if not os.path.exists(predict_dir):
+    os.mkdir(predict_dir)
 
 Saver =tensorflow.train.import_meta_graph(tensorflow.train.latest_checkpoint(model_dir)+'.meta')
 Session=tensorflow.Session()
@@ -21,17 +28,26 @@ for x in rads:
     image_dir=[os.path.join(x,y) for y in os.listdir(x)]
     image_dir.sort()
     all_image_dir.append(image_dir)
+
 for x in all_image_dir:
-    k1=[]
-    for y in x:
-        k1.append(cv2.imread(y))
-    k1=numpy.array(k1).reshape([1,31,501,501,3])
-    predict_result=Session.run(cnn_decode_result, feed_dict={train_image:k1[:,:,:,:,0:1]})
-    predict_result=predict_result[0,:,:,:,:]
-    predict_result=predict_result.repeat(3,axis=3)
-    folder_name=os.path.split(os.path.split(y)[0])[1]
-    save_root=os.path.join(os.path.split(data_dir)[0],'predict',folder_name)
-    name1=os.path.split(y)[1]
-    for z in range(6):
-        result=predict_result[5*z+4]
-        cv2.imwrite(save_root, predict_result)
+    try:
+        k1=[]
+        for y in x:
+            k1.append(skimage.io.imread(y))
+        k1=numpy.array(k1).reshape([1,31,501,501,1])
+        k1=k1.repeat(3,4)
+        predict_result=Session.run(cnn_decode_result, feed_dict={train_image:k1[:,:,:,:,0:1]})
+        predict_result=predict_result[0,:,:,:,:]
+        predict_result=predict_result.repeat(3,axis=3)
+        folder_name=os.path.split(os.path.split(y)[0])[1]
+        save_root=os.path.join(predict_dir,folder_name)
+        if not os.path.exists(save_root):
+            os.mkdir(save_root)
+        for z in range(6):
+            name=folder_name+'_f00%s.png'%(z+1)
+            result=predict_result[5*z+4]
+            cv2.imwrite(os.path.join(save_root,name), result)
+    except:
+        print(y)
+
+zipfile.shutil.make_archive(base_name=predict_dir,format='zip',root_dir=predict_dir)
