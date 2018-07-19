b216e03250c511f0853b6faf1b7ef65a6b94f191
zhaojinxi <super-string@outlook.com>
2018-07-19 19:19:44 +0800

    6

--- compete_tianchi/全球气象AI挑战赛/convlstm/convlstm2.py
+++ compete_tianchi/全球气象AI挑战赛/convlstm/convlstm2.py
@@ -3,7 +3,6 @@ import numpy
 import os
 import random
 import time
-import cv2
 import skimage.io
 
 # data_dir='E:/SRAD2018/train'
@@ -17,53 +16,34 @@ batch_file=1
 batch_rad=1
 batch_size=batch_file*batch_rad
 max_step=300000//batch_size+1
-
-def gru_process(input_code):
-    all_output_encode=[]
-    init_hide=numpy.zeros([batch_size,32,32,32]).astype(numpy.float32)
-    for i in range(31):
-        if i==0:
-            output_hide=convgru_encode(init_hide,input_code[:,i,:,:,:])
-            all_output_encode.append(output_hide)
-        else:
-            output_hide=convgru_encode(output_hide,input_code[:,i,:,:,:])
-            all_output_encode.append(tensorflow.reshape(output_hide,[batch_size,1,32,32,32]))
-
-    all_output_decode=[]
-    for i in range(30):
-        output_hide=convgru_decode(output_hide)
-        all_output_decode.append(output_hide)
-
-    return all_output_encode, all_output_decode
-
-len([x.name for x in tensorflow.get_collection(tensorflow.GraphKeys.GLOBAL_VARIABLES)])
+channel_dim1=4
 
 train_image=tensorflow.placeholder(tensorflow.float32,[batch_size,31,501,501,1],name='train_image')
 answer_image=tensorflow.placeholder(tensorflow.float32,[batch_size,30,501,501,1],name='answer_image')
 global_step = tensorflow.get_variable('global_step',initializer=0,trainable=False)
-learning_rate=tensorflow.train.exponential_decay(init_lr,global_step,max_step*30,decay_rate)
-which_opt = tensorflow.get_variable('which_opt',initializer=0,trainable=False)
+learning_rate=tensorflow.train.exponential_decay(init_lr,global_step,max_step,decay_rate)
+which_loss = tensorflow.get_variable('which_loss',initializer=0,trainable=False)
 
-encoder_cell = tensorflow.contrib.rnn.Conv2DLSTMCell(input_shape=[batch_size,501,501],output_channels=4,kernel_shape=[3,3],use_bias=True,forget_bias=True,initializers=tensorflow.truncated_normal_initializer,name='encoder_convlstm1')
-encoder_outputs, encoder_final_state = tensorflow.nn.dynamic_rnn(
-        encoder_cell, train_image,
-        dtype=tensorflow.float32, time_major=True,)
-decoder_cell = tensorflow.contrib.rnn.Conv2DLSTMCell(input_shape=[batch_size,501,501],output_channels=4,kernel_shape=[3,3],use_bias=True,forget_bias=True,initializers=tensorflow.truncated_normal_initializer,name='decoder_convlstm1')
-decoder_outputs, decoder_final_state = tensorflow.nn.dynamic_rnn(
-        decoder_cell, answer_image,
-        initial_state=encoder_final_state,
-        dtype=tensorflow.float32, time_major=True, scope="plain_decoder",)
+encoder_cell = tensorflow.contrib.rnn.ConvLSTMCell(conv_ndims=2,input_shape=[501,501,1], output_channels=channel_dim1, kernel_shape=[3,3], use_bias=True, forget_bias=True, initializers=tensorflow.truncated_normal_initializer(stddev=0.1), name='encode_convlstm1')
+encoder_outputs, encoder_final_state = tensorflow.nn.dynamic_rnn(encoder_cell, train_image, dtype=tensorflow.float32, scope='encode')
+decoder_cell = tensorflow.contrib.rnn.ConvLSTMCell(conv_ndims=2, input_shape=[501,501,1], output_channels=channel_dim1, kernel_shape=[3,3], use_bias=True, forget_bias=True, initializers=tensorflow.truncated_normal_initializer(stddev=0.1), name='decode_convlstm1')
+decoder_outputs, decoder_final_state = tensorflow.nn.dynamic_rnn(decoder_cell, encoder_outputs, initial_state=encoder_final_state, scope='decode')
 
-cnn_encode_result=tensorflow.map_fn(cnn_encode,train_image,name='cnn_encode_result')
-gru_result=gru_process(cnn_encode_result)
-pre_result=tensorflow.stack(gru_result[1],1)
-cnn_decode_result=tensorflow.map_fn(cnn_decode,pre_result,name='cnn_decode_result')
+def predict(x):
+    with tensorflow.variable_scope('predict',reuse=tensorflow.AUTO_REUSE):
+        w1=tensorflow.get_variable('w1', [3,3,channel_dim1,1], initializer=tensorflow.truncated_normal_initializer(stddev=0.1))
+        b1=tensorflow.get_variable('b1', 1, initializer=tensorflow.constant_initializer(0))
+        z1=tensorflow.nn.tanh(tensorflow.nn.conv2d(x,w1,[1,1,1,1],'SAME')+b1)
+        z1=tensorflow.clip_by_value(z1*128+128,0,255)
+    return z1
 
-loss=tensorflow.losses.mean_squared_error(answer_image[:,which_opt,:,:,:],cnn_decode_result[:,which_opt,:,:,:])
+decode_image=tensorflow.map_fn(predict,decoder_outputs,name='decode_image')
+
+loss=tensorflow.losses.mean_squared_error(answer_image,decode_image)
 
 minimize=tensorflow.train.AdamOptimizer(learning_rate).minimize(loss,global_step=global_step,name='minimize')
 
-Saver = tensorflow.train.Saver(max_to_keep=0,filename='cnn_convgru')
+Saver = tensorflow.train.Saver(max_to_keep=0,filename='convlstm')
 
 Session=tensorflow.Session()
 if tensorflow.train.latest_checkpoint(model_dir):
@@ -73,7 +53,7 @@ else:
 
 tensorflow.summary.scalar('loss', loss)
 tensorflow.summary.image('answer_images', answer_image[0,:,:,:,:], 10)
-tensorflow.summary.image('output_images', cnn_decode_result[0], 10)
+tensorflow.summary.image('output_images', decode_image[0], 10)
 merge_all = tensorflow.summary.merge_all()
 FileWriter = tensorflow.summary.FileWriter(log_dir, Session.graph)
 
@@ -96,15 +76,14 @@ for _ in range(max_step):
         for y in x:
             k1.append(skimage.io.imread(y))
         all_image.append(k1)
-    all_image=numpy.array(all_image)
+    all_image=numpy.array(all_image).reshape(batch_size,61,501,501,1)
     try:
-        for j in range(30):
-            Session.run(minimize,feed_dict={train_image:all_image[:,:31,:,:,0:1],answer_image:all_image[:,31:,:,:,0:1],which_opt:j})
-        if Session.run(global_step)%3000==30:
-            summary = Session.run(merge_all, feed_dict={train_image:all_image[:,:31,:,:,0:1],answer_image:all_image[:,31:,:,:,0:1],which_opt:10})
+        Session.run(minimize,feed_dict={train_image:all_image[:,:31,:,:,:],answer_image:all_image[:,31:,:,:,:]})
+        if Session.run(global_step)%100==1:
+            summary = Session.run(merge_all, feed_dict={train_image:all_image[:,:31,:,:,:], answer_image:all_image[:,31:,:,:,:]}) 
             FileWriter.add_summary(summary, Session.run(global_step))
             Saver.save(Session, model_dir, global_step)
-            print(Session.run(loss,feed_dict={train_image:all_image[:,:31,:,:,0:1],answer_image:all_image[:,31:,:,:,0:1]}))
+            print(Session.run(loss,feed_dict={train_image:all_image[:,:31,:,:,:],answer_image:all_image[:,31:,:,:,:]}))
     except:
         with open('log/异常数据目录.txt','a') as f:
             f.write('异常数据:%s\n'%(rads))
