d855a9d85169ae6d89acc903a3e9b8cfe5323a97
zhaojinxi <super-string@outlook.com>
2018-12-15 22:11:25 +0800

    6

---
+++ learn_tensorflow/Resources/Probability/OVERVIEW/TensorFlow Probability is a library for probabilistic reasoning and statistical analysis.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+# Pretend to load synthetic data set.
+features = tfp.distributions.Normal(loc=0., scale=1.).sample(int(100e3))
+labels = tfp.distributions.Bernoulli(logits=1.618 * features).sample()
+
+# Specify model.
+model = tfp.glm.Bernoulli()
+
+# Fit model given data.
+coeffs, linear_response, is_converged, num_iter = tfp.glm.fit(
+    model_matrix=features[:, tf.newaxis],
+    response=tf.to_float(labels),
+    model=model)
+# ==> coeffs is approximately [1.618] (We're golden!)
