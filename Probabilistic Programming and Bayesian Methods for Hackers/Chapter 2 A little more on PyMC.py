a33e331d4612bc4f70222ea0c36b381284e2216d
zhaojnixi <super-string@outlook.com>
2019-01-02 19:49:26 +0800

    6

---
+++ Probabilistic Programming and Bayesian Methods for Hackers/Chapter 2 A little more on PyMC.py
@@ -0,0 +1,126 @@
+#@title Imports and Global Variables (run this cell first)  { display-mode: "form" }
+"""
+The book uses a custom matplotlibrc file, which provides the unique styles for
+matplotlib plots. If executing this book, and you wish to use the book's
+styling, provided are two options:
+    1. Overwrite your own matplotlibrc file with the rc-file provided in the
+       book's styles/ dir. See http://matplotlib.org/users/customizing.html
+    2. Also in the styles is  bmh_matplotlibrc.json file. This can be used to
+       update the styles in only this notebook. Try running the following code:
+
+        import json
+        s = json.load(open("../styles/bmh_matplotlibrc.json"))
+        matplotlib.rcParams.update(s)
+"""
+# !pip3 install -q wget
+from __future__ import absolute_import, division, print_function
+#@markdown This sets the warning status (default is `ignore`, since this notebook runs correctly)
+warning_status = "ignore" #@param ["ignore", "always", "module", "once", "default", "error"]
+import warnings
+warnings.filterwarnings(warning_status)
+with warnings.catch_warnings():
+    warnings.filterwarnings(warning_status, category=DeprecationWarning)
+    warnings.filterwarnings(warning_status, category=UserWarning)
+import numpy as np
+import os
+#@markdown This sets the styles of the plotting (default is styled like plots from [FiveThirtyeight.com](https://fivethirtyeight.com/))
+matplotlib_style = 'fivethirtyeight' #@param ['fivethirtyeight', 'bmh', 'ggplot', 'seaborn', 'default', 'Solarize_Light2', 'classic', 'dark_background', 'seaborn-colorblind', 'seaborn-notebook']
+import matplotlib.pyplot as plt; plt.style.use(matplotlib_style)
+import matplotlib.axes as axes;
+from matplotlib.patches import Ellipse
+# %matplotlib inline
+import seaborn as sns; sns.set_context('notebook')
+from IPython.core.pylabtools import figsize
+#@markdown This sets the resolution of the plot outputs (`retina` is the highest resolution)
+notebook_screen_res = 'retina' #@param ['retina', 'png', 'jpeg', 'svg', 'pdf']
+# %config InlineBackend.figure_format = notebook_screen_res
+import tensorflow as tf
+tfe = tf.contrib.eager
+# Eager Execution
+#@markdown Check the box below if you want to use [Eager Execution](https://www.tensorflow.org/guide/eager)
+#@markdown Eager execution provides An intuitive interface, Easier debugging, and a control flow comparable to Numpy. You can read more about it on the [Google AI Blog](https://ai.googleblog.com/2017/10/eager-execution-imperative-define-by.html)
+use_tf_eager = False #@param {type:"boolean"}
+# Use try/except so we can easily re-execute the whole notebook.
+if use_tf_eager:
+    try:
+        tf.enable_eager_execution()
+    except:
+        pass
+import tensorflow_probability as tfp
+tfd = tfp.distributions
+tfb = tfp.bijectors
+def evaluate(tensors):
+    """Evaluates Tensor or EagerTensor to Numpy `ndarray`s.
+    Args:
+    tensors: Object of `Tensor` or EagerTensor`s; can be `list`, `tuple`,
+        `namedtuple` or combinations thereof.
+
+    Returns:
+        ndarrays: Object with same structure as `tensors` except with `Tensor` or
+          `EagerTensor`s replaced by Numpy `ndarray`s.
+    """
+    if tf.executing_eagerly():
+        return tf.contrib.framework.nest.pack_sequence_as(
+            tensors,
+            [t.numpy() if tf.contrib.framework.is_tensor(t) else t
+             for t in tf.contrib.framework.nest.flatten(tensors)])
+    return sess.run(tensors)
+class _TFColor(object):
+    """Enum of colors used in TF docs."""
+    red = '#F15854'
+    blue = '#5DA5DA'
+    orange = '#FAA43A'
+    green = '#60BD68'
+    pink = '#F17CB0'
+    brown = '#B2912F'
+    purple = '#B276B2'
+    yellow = '#DECF3F'
+    gray = '#4D4D4D'
+    def __getitem__(self, i):
+        return [
+            self.red,
+            self.orange,
+            self.green,
+            self.blue,
+            self.pink,
+            self.brown,
+            self.purple,
+            self.yellow,
+            self.gray,
+        ][i % 9]
+TFColor = _TFColor()
+def session_options(enable_gpu_ram_resizing=True, enable_xla=True):
+    """
+    Allowing the notebook to make use of GPUs if they're available.
+    
+    XLA (Accelerated Linear Algebra) is a domain-specific compiler for linear 
+    algebra that optimizes TensorFlow computations.
+    """
+    config = tf.ConfigProto()
+    config.log_device_placement = True
+    if enable_gpu_ram_resizing:
+        # `allow_growth=True` makes it possible to connect multiple colabs to your
+        # GPU. Otherwise the colab malloc's all GPU ram.
+        config.gpu_options.allow_growth = True
+    if enable_xla:
+        # Enable on XLA. https://www.tensorflow.org/performance/xla/.
+        config.graph_options.optimizer_options.global_jit_level = (
+            tf.OptimizerOptions.ON_1)
+    return config
+def reset_sess(config=None):
+    """
+    Convenience function to create the TF graph & session or reset them.
+    """
+    if config is None:
+        config = session_options()
+    global sess
+    tf.reset_default_graph()
+    try:
+        sess.close()
+    except:
+        pass
+    sess = tf.InteractiveSession(config=config)
+reset_sess()
+
+#A little more on TensorFlow and TensorFlow Probability
+
