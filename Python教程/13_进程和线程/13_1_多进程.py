d16bdcc9d4e29c885604645d4b89b7a4aa45d388
zhaojinxi <super-string@outlook.com>
2019-02-19 01:18:35 +0800

    6

--- Python教程/13_进程和线程/13_1_多进程.py
+++ Python教程/13_进程和线程/13_1_多进程.py
@@ -39,15 +39,13 @@ if __name__=='__main__':
     p.join()
     print('All subprocesses done.')
 
-print('$ nslookup www.python.org')
 r = subprocess.call(['nslookup', 'www.python.org'])
-print('Exit code:', r)
+print('subprocess.call:', r)
 
-print('$ nslookup')
 p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 output, err = p.communicate(b'set q=mx\npython.org\nexit\n')
 print(output.decode('gb18030'))
-print('Exit code:', p.returncode)
+print('subprocess.Popen:', p.returncode)
 
 # 写数据进程执行的代码:
 def write(q):
