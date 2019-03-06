ffb9059de28a104346a5d5550636a6378497e848
zhaojnixi <super-string@outlook.com>
2019-03-06 14:45:36 +0800

    6

--- learn_tensorflow/learning_rate.py
+++ learn_tensorflow/learning_rate.py
@@ -11,8 +11,9 @@ noisy_linear_cosine_decay = tensorflow.train.noisy_linear_cosine_decay(learning_
 cosine_decay = tensorflow.train.cosine_decay(learning_rate=0.1, global_step=global_step, decay_steps=150, alpha=0.01, name='cosine_decay')
 cosine_decay_restarts = tensorflow.train.cosine_decay_restarts(learning_rate=0.1, global_step=global_step, first_decay_steps=30, t_mul=3, m_mul=0.5, alpha=0.01, name='cosine_decay_restarts')
 polynomial_decay = tensorflow.train.polynomial_decay(learning_rate=0.1, global_step=global_step, decay_steps=100, end_learning_rate=0.001, power=2, cycle=True, name='polynomial_decay')
+piecewise_constant_decay = tensorflow.train.piecewise_constant_decay(x=global_step, boundaries=[100, 200], values=[0.1, 0.01, 0.001], name='piecewise_constant_decay')
 
-all_decay = [exponential_decay, natural_exp_decay, inverse_time_decay, linear_cosine_decay, noisy_linear_cosine_decay, cosine_decay, cosine_decay_restarts, polynomial_decay]
+all_decay = [exponential_decay, natural_exp_decay, inverse_time_decay, linear_cosine_decay, noisy_linear_cosine_decay, cosine_decay, cosine_decay_restarts, polynomial_decay, piecewise_constant_decay]
 
 Session = tensorflow.Session()
 Session.run(tensorflow.global_variables_initializer())
